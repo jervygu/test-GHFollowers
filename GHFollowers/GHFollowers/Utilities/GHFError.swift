661d19355da272086efbac676d00144833c5b109
Jervy Umandap <jegumandap@unionbankph.com>
2024-07-19 17:50:26 +0800

    ### Result Type Refactor
    * Swift's Result Type

--- GHFollowers/GHFollowers/Utilities/GHFError.swift
+++ GHFollowers/GHFollowers/Utilities/GHFError.swift
@@ -1,5 +1,5 @@
 //
-//  ErrorMessage.swift
+//  GHFError.swift
 //  GHFollowers
 //
 //  Created by Jervy Umandap on 7/19/24.
@@ -7,7 +7,7 @@
 
 import Foundation
 
-enum ErrorMessage: String {
+enum GHFError: String, Error {
     case invalidUsername = "This username created an invalid request, please try again."
     case unableToComplete = "Unable to complete your request, please check internet connection"
     case invalidResponse = "Invalid response from the server, please try again"
