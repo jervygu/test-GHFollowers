89b52b4f20f7afa6d1edf62307e0fdd4ee5ebf6e
Jervy Umandap <jegumandap@unionbankph.com>
2024-07-19 08:52:04 +0800

    Starter project

---
+++ GHFollowers/GHFollowers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GHFollowers
+//
+//  Created by Jervy Umandap on 7/19/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
