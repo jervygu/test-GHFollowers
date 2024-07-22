86c0952bbf8721377fda612ae2c9a2eb0482f09e
Jervy Umandap <jegumandap@unionbankph.com>
2024-07-22 16:00:37 +0800

    ### UserInfoVC - UserInfoHeaderVC
    * Child ViewControllers
    * ViewControllers vs. UIViews
    * SecondaryTitleLabel
    * SFSymbols
    * Constants File

--- GHFollowers/GHFollowers/Screens/UserInfoVC.swift
+++ GHFollowers/GHFollowers/Screens/UserInfoVC.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class UserInfoVC: UIViewController {
     
+    let headerView = UIView()
     var username: String!
     
 //    init(username: String) {
@@ -32,6 +33,7 @@ class UserInfoVC: UIViewController {
         let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dismissVC))
         navigationItem.rightBarButtonItem = doneButton
         
+        layoutUI()
         getUser()
     }
     
@@ -40,11 +42,32 @@ class UserInfoVC: UIViewController {
             guard let self = self else { return }
             switch result {
             case .success(let user):
-                print(user)
+                DispatchQueue.main.async {
+                    self.add(childVC: GHFUserInfoHeaderVC(user: user), to: self.headerView)
+                }
             case .failure(let error):
                 presentGHFAlertOnMainThread(title: "Something went wrong!", message: error.rawValue, buttonTitle: "Ok")
             }
         }
+    }
+    
+    func layoutUI() {
+        view.addSubview(headerView)
+        headerView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            headerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            headerView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            headerView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            headerView.heightAnchor.constraint(equalToConstant: 180),
+        ])
+    }
+    
+    func add(childVC: UIViewController, to containerView: UIView) {
+        addChild(childVC)
+        containerView.addSubview(childVC.view)
+        childVC.view.frame = containerView.bounds
+        childVC.didMove(toParent: self)
     }
     
     @objc func dismissVC() {
---
+++ GHFollowers/GHFollowers/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  GHFollowers
+//
+//  Created by Jervy Umandap on 7/22/24.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+    
+}
